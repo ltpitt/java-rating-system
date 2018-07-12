65365bd86fbeae70393764d46b28ed69c6298bb3
Davide Nastri <d.nastri@gmail.com>
2018-07-12 12:41:42 +0200

    feat: Implemented SecondRatings class

---
+++ SecondRatings.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of SecondRatings here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+import java.util.*;
+
+public class SecondRatings {
+    private ArrayList<Movie> myMovies;
+    private ArrayList<Rater> myRaters;
+
+    public SecondRatings(String moviefile, String ratingsfile){
+        FirstRatings fR=new FirstRatings();
+        myMovies=fR.loadMovies(moviefile);
+        myRaters=fR.loadRaters(ratingsfile);
+    }
+
+    public SecondRatings() {
+        // Default constructor
+        this("ratedmoviesfull.csv", "ratings.csv");
+    }
+    
+    public int getMovieSize(){
+        return myMovies.size();
+    }
+
+    public int getRaterSize(){
+        return myRaters.size();
+    }
+
+}
