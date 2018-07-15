04b68c51e20e44d6a80200d7fd10adb2975b8160
ltpitt <d.nastri@gmail.com>
2018-07-15 14:50:54 +0200

    feat: Implemented printAverageRatings() in MovieRunnerAverage class

--- MovieRunnerAverage.java
+++ MovieRunnerAverage.java
@@ -1,4 +1,16 @@
 import java.util.*;
 
 public class MovieRunnerAverage {
+
+   public void printAverageRatings(){
+       SecondRatings sr = new SecondRatings("data/ratedmovies_short.csv", "data/ratings_short.csv");
+       System.out.println(sr.getMovieSize());
+       System.out.println(sr.getRaterSize());
+
+   }
+
+   public static void main(String args[]){
+       MovieRunnerAverage mra = new MovieRunnerAverage();
+       mra.printAverageRatings();
+   }
 }
