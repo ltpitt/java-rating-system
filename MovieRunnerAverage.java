6868e2a125b794231832d3780b71bb0ceec11b42
Davide Nastri <d.nastri@gmail.com>
2018-07-18 17:47:19 +0200

    feat: added getTitle() to MovieRunnerAverage class

--- MovieRunnerAverage.java
+++ MovieRunnerAverage.java
@@ -1,14 +1,16 @@
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class MovieRunnerAverage {
 
    public void printAverageRatings(){
-       //SecondRatings sr = new SecondRatings();
-       SecondRatings sr = new SecondRatings("data/ratedmovies_short.csv", "data/ratings_short.csv");
+       SecondRatings sr = new SecondRatings();
+       //SecondRatings sr = new SecondRatings("data/ratedmovies_short.csv", "data/ratings_short.csv");
        //sr.getMovieSize();
        //sr.getRaterSize();
        //sr.getAverageByID("0006414",1);
-       sr.getAverageRatings(10);
+       //sr.getAverageRatings(10);
+       sr.getTitle("0006414");
    }
 
    public static void main(String args[]){
