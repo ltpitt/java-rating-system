3e305f70b02bc08e80844fcb94f383e1aaadc126
ltpitt <d.nastri@gmail.com>
2018-07-22 17:41:43 +0200

    feat: Implemented getAverageRatingOneMovie() in MovieRunnerAverage class

--- MovieRunnerAverage.java
+++ MovieRunnerAverage.java
@@ -4,29 +4,41 @@ import java.util.*;
 public class MovieRunnerAverage {
 
    public void printAverageRatings(){
-       //SecondRatings sr = new SecondRatings();
-       SecondRatings sr = new SecondRatings("data/ratedmovies_short.csv", "data/ratings_short.csv");
+       SecondRatings sr = new SecondRatings();
+       //SecondRatings sr = new SecondRatings("data/ratedmovies_short.csv", "data/ratings_short.csv");
        //sr.getMovieSize();
        //sr.getRaterSize();
+       //String ID = sr.getID("The Godfather");
+       //sr.getAverageByID(ID,1);
        //sr.getAverageByID("0006414",1);
        //sr.getAverageRatings(10);
        //sr.getTitle("0006414");
        ArrayList<Rating> averageRatingsAL = new ArrayList<Rating>();
-       averageRatingsAL = sr.getAverageRatings(3);
+       averageRatingsAL = sr.getAverageRatings(12);
 
        Collections.sort(averageRatingsAL);
 
        for (Rating currentRating : averageRatingsAL){
            System.out.println(currentRating.getValue() + " " + sr.getTitle(currentRating.getItem()));
+           break;
        }
 
+        //System.out.println(averageRatingsAL.size());
    }
 
+    public void getAverageRatingOneMovie(){
+        SecondRatings sr = new SecondRatings();
+        //SecondRatings sr = new SecondRatings("data/ratedmovies_short.csv", "data/ratings_short.csv");
+        System.out.println("\ngetAverageRatingOneMovie():");
+        String ID = sr.getID("Vacation");
+        sr.getAverageByID(ID, 1);
 
+    }
 
 
    public static void main(String args[]){
        MovieRunnerAverage mra = new MovieRunnerAverage();
        mra.printAverageRatings();
+       mra.getAverageRatingOneMovie();
    }
 }
