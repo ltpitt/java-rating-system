9a948e6e539689a167febe7ce5d8defff1d8a57e
ltpitt <d.nastri@gmail.com>
2018-08-05 10:35:56 +0200

    feat: Implemented printAverageRatingsByYear() in MovieRunnerWithFilters class

--- MovieRunnerWithFilters.java
+++ MovieRunnerWithFilters.java
@@ -32,12 +32,35 @@ public class MovieRunnerWithFilters {
         sr.getAverageByID(ID, 1);
 
     }
+    
+    public void printAverageRatingsByYear() {
+        //ThirdRatings tr = new ThirdRatings();
+        ThirdRatings tr = new ThirdRatings("data/ratings_short.csv");
+        System.out.println("Read data for \t" + tr.getRaterSize() + " raters");
+        System.out.println("Read data for \t" + MovieDatabase.size() + " movies" );
+
+        YearAfterFilter ya = new YearAfterFilter(2000);
+        ArrayList<Rating> averageRatingsAL = new ArrayList<Rating>();
+        averageRatingsAL = tr.getAverageRatingsByFilter(1, ya);
+        Collections.sort(averageRatingsAL);
+
+        System.out.println("Found \t" + averageRatingsAL.size() + " movies" );
+        for (Rating currentRating : averageRatingsAL){
+            double currentRatingAverage = currentRating.getValue();
+            String currentMovieID = currentRating.getItem();
+            int currentYear = MovieDatabase.getYear(currentMovieID);
+            String currentTitle = MovieDatabase.getMovie(currentMovieID).getTitle();
+            System.out.println(currentRatingAverage + " " + currentYear + " " + currentTitle);
+        }
 
 
-   public static void main(String args[]){
+    }
+
+    public static void main(String args[]){
        MovieRunnerWithFilters mra = new MovieRunnerWithFilters();
        MovieDatabase.initialize("ratedmovies_short.csv");
        //mra.getAverageRatingOneMovie();
-       mra.printAverageRatings();
+       //mra.printAverageRatings();
+       mra.printAverageRatingsByYear();
    }
 }
