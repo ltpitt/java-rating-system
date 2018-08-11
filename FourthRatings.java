a2473e23c4474e14d3e7d55261e760b104b18f23
ltpitt <d.nastri@gmail.com>
2018-08-11 20:48:18 +0200

    feat: Implemented printAverageRatingsByYear() in MovieRunnerWithFilters class

--- FourthRatings.java
+++ FourthRatings.java
@@ -2,8 +2,8 @@
 /**
  * Write a description of SecondRatings here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Davide Nastri
+ * @version 8/11/2018
  */
 
 import java.util.ArrayList;
